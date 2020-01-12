d87193373a878ee711b173da71dbd9af04820cd2
Huy Canh <huycanh14@gmail.com>
2020-01-13 01:05:39 +0700

    Add file sql and Finished Website TinTuc

---
+++ Controllers/DanhMucController.cs
@@ -0,0 +1,39 @@
+﻿using PagedList;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebTinTuc.Models;
+
+namespace WebTinTuc.Controllers
+{
+    public class DanhMucController : Controller
+    {
+        WebTinTucModel db = new WebTinTucModel();
+        // GET: DanhMuc
+        public ActionResult Index(int page = 1,int id = 0)
+        {
+            var data = db.DanhMucs.Find(id);
+            if (data == null)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            ViewBag.Title = data.TenDanhMuc;
+            var TinTucs = (from tt in db.TinTucs
+                              join dm in db.DanhMucs on tt.IdDanhMuc equals dm.Id
+                              orderby tt.Id descending
+                              where tt.IdDanhMuc == id
+                              select new TinTuc_DanhMuc
+                              {
+                                  DanhMuc = dm.TenDanhMuc,
+                                  MaDanhMuc = dm.Id,
+                                  MaTinTuc = tt.Id,
+                                  TieuDe = tt.TieuDe,
+                                  NgayTao = tt.NgayTao,
+                                  HinhAnh = tt.HinhAnh
+                              }).ToPagedList(page, 12);
+            return View(TinTucs);
+        }
+    }
+}
---
+++ Models/DanhGia_TinTuc.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebTinTuc.Models
+{
+    public class DanhGia_TinTuc
+    {
+        public int Id { get; set; }
+        public string NoiDung { get; set; }
+        public string TinTuc { get; set; }
+        public string TaiKhoan { get; set; }
+        public DateTime NgayTao { get; set; }
+        public DateTime NgaySua { get; set; }
+        public bool TrangThai { get; set; }
+
+        /*object data = (from cmt in db.DanhGias
+                       join tt in db.TinTucs on cmt.IdTinTuc equals tt.Id
+                       join tk in db.TaiKhoans on cmt.IdTaiKhoan equals tk.Id
+                       select new DanhGia_TinTuc
+                       {
+                           Id = cmt.Id,
+                           NoiDung = cmt.NoiDung,
+                           TinTuc = tt.TieuDe,
+                           TaiKhoan = tk.HoTen,
+                           NgayTao = cmt.NgayTao,
+                           NgaySua = cmt.NgaySua,
+                           TrangThai = cmt.TrangThai
+                       }).ToList();*/
+    }
+}
